5c75a325d9a6471b002f6a8ed24534aea57a4c10
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-12-06 11:11:11 +0000

    Etapa 7

--- js/script-index.js
+++ js/script-index.js
@@ -5,6 +5,7 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
+
 });
 
 function printNews(){
--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
 	$('a.js-menu').css("display", "none");
+	console.log('Está listo');
 });
