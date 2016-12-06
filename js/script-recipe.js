59b01a85d684c4c1ef72b89af7c89ed17a1ad0be
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-12-06 11:23:53 +0000

    Estapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 	$('a.js-menu').css("display", "none");
 	console.log('Está listo');
+
+	$('a.js-show-recipe').click(function(){
+		$('div.page.recipe').removeClass('make');
+	});
+
+	$('a.js-show-make').click(function(){
+		$('div.page.recipe').addClass('make');
+	});
 });
+
